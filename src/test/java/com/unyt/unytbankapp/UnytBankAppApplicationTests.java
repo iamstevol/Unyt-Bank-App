e59f0a9f3d74c66ac61ed31a850fdd4119d88388
iamstevol <stevol2015@gmail.com>
2023-06-15 00:27:45 +0100

    New commit

---
+++ src/test/java/com/unyt/unytbankapp/UnytBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unyt.unytbankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnytBankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
