968440dc7e23e451d95916d4a9eebb36c24f193f
iamstevol <stevol2015@gmail.com>
2023-06-15 23:23:17 +0100

    Email metadata

--- src/main/java/com/unyt/unytbankapp/controller/UserController.java
+++ src/main/java/com/unyt/unytbankapp/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.unyt.unytbankapp.controller;
 
+import com.unyt.unytbankapp.dto.BankResponse;
+import com.unyt.unytbankapp.dto.UserRequest;
+import com.unyt.unytbankapp.service.impl.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/createAccount")
+    private BankResponse createAccount(@RequestBody UserRequest userRequest) {
+        return userService.createAccount(userRequest);
+    }
 }
--- src/main/java/com/unyt/unytbankapp/entity/User.java
+++ src/main/java/com/unyt/unytbankapp/entity/User.java
@@ -1,8 +1,6 @@
 package com.unyt.unytbankapp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -20,6 +18,7 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
