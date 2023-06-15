1e68f849064e805cfb5a137dcaa37636094aced2
iamstevol <stevol2015@gmail.com>
2023-06-15 08:39:45 +0100

    User metadata continuation

---
+++ src/main/java/com/unyt/unytbankapp/dto/UserResponse.java
@@ -0,0 +1,4 @@
+package com.unyt.unytbankapp.dto;
+
+public class UserResponse {
+}
---
+++ src/main/java/com/unyt/unytbankapp/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.unyt.unytbankapp.repository;
+
+import com.unyt.unytbankapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
