d9a1a536f2aa3ae4537abe3dc2ab8ab025e3deb2
iamstevol <stevol2015@gmail.com>
2023-06-15 09:04:51 +0100

    Business Logic continuation

--- src/main/java/com/unyt/unytbankapp/service/impl/UserServiceImpl.java
+++ src/main/java/com/unyt/unytbankapp/service/impl/UserServiceImpl.java
@@ -2,11 +2,20 @@ package com.unyt.unytbankapp.service.impl;
 
 import com.unyt.unytbankapp.dto.BankResponse;
 import com.unyt.unytbankapp.dto.UserRequest;
+import com.unyt.unytbankapp.entity.User;
 
 public class UserServiceImpl implements UserService {
 
     @Override
     public BankResponse createAccount(UserRequest userRequest) {
-        return null;
+        User newUser = User.builder()
+                .firstName(userRequest.getFirstName())
+                .lastName(userRequest.getLastName())
+                .otherName(userRequest.getOtherName())
+                .gender(userRequest.getGender())
+                .address(userRequest.getAddress())
+                .stateOfOrigin(userRequest.getStateOfOrigin())
+                .accountNumber()
+                .build();
     }
 }
