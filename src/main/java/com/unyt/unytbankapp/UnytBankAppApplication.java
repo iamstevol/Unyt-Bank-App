af0fe2b0caa539a6db5a4d59db9ca0451f6e588c
iamstevol <stevol2015@gmail.com>
2023-06-21 07:50:47 +0100

    Added a documentation

--- src/main/java/com/unyt/unytbankapp/UnytBankAppApplication.java
+++ src/main/java/com/unyt/unytbankapp/UnytBankAppApplication.java
@@ -1,9 +1,34 @@
 package com.unyt.unytbankapp;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Unyt Bank Application",
+                description = "Backend rest API for unyt bank app",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Stephen Oluboyo",
+                        email = "stevol2015@gmail.com",
+                        url = "https://github.com/iamstevol/unyt_bank_app"
+                ),
+                license = @License(
+                        name = "Unyt Bank App",
+                        url = "https://github.com/iamstevol/unyt_bank_app"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Unyt bank App Documentation",
+                url = "https://github.com/iamstevol/unyt_bank_app"
+        )
+)
 public class UnytBankAppApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/unyt/unytbankapp/controller/UserController.java
+++ src/main/java/com/unyt/unytbankapp/controller/UserController.java
@@ -2,21 +2,41 @@ package com.unyt.unytbankapp.controller;
 
 import com.unyt.unytbankapp.dto.*;
 import com.unyt.unytbankapp.service.impl.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Tag(name = "User Account Management APIs")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(
+            summary = "Create New User Account",
+            description = "Creating a new user and assigning an account ID"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
     @PostMapping("/createAccount")
     private BankResponse createAccount(@RequestBody UserRequest userRequest) {
         return userService.createAccount(userRequest);
     }
 
+    @Operation(
+            summary = "Balance Enquiry",
+            description = "Given an account number, check the account balance"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("/balanceEnquiry")
     private BankResponse balanceEnquiry(@RequestBody EnquiryRequest request) {
         return userService.balanceEnquiry(request);
