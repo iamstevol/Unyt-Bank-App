41e27b6a52e5509d6f82092787f0fb45a10fa0a9
iamstevol <stevol2015@gmail.com>
2023-06-21 11:11:26 +0100

    Created a transaction entity

---
+++ src/main/java/com/unyt/unytbankapp/entity/Transaction.java
@@ -0,0 +1,24 @@
+package com.unyt.unytbankapp.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String transactionId;
+    private String transactionType;
+    private BigDecimal amount;
+    private String accountNumber;
+}
